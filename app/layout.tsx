ccaddd6b9d008bcc0bbdf0325681736e84643d0b
mario <mario@marios-MacBook-Pro.local>
2024-10-18 20:49:36 +0700

    added adsense footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,15 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <title>Drum Machine</title>
         <meta name="description" content={metadata.description ?? "Default Description"} />
+        <meta name="keywords" content="drum machine, music app, audio, sound, drum kits, music production" />
+        <meta name="author" content="Your Name or Company" />
+        <meta property="og:title" content="Drum Machine" />
+        <meta property="og:description" content={metadata.description ?? "Default Description"} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://drrrumm.ing" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Drum Machine" />
+        <meta name="twitter:description" content={metadata.description ?? "Default Description"} />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2235699957137388" crossOrigin="anonymous"></script>
       </head>
       <body
